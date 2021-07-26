f7ecba645b04d7d0207387fdba1e348aaa8ca893
genhk12 <fannyloveseggs@gmail.com>
2021-07-26 17:30:11 +0800

    Added JS Assignments

---
+++ codecademy_exercise/JS-Dog_Years/index.js
@@ -0,0 +1,19 @@
+//myAge = 20
+const myAge = 20;
+
+//earlyYears = 2
+let earlyYears = 2;
+
+earlyYears = earlyYears * 10.5;
+//comment
+let laterYears = myAge - 2;
+//comment
+laterYears = laterYears * 4;
+console.log(`early Years: ${earlyYears}`)
+console.log(`later Years: ${laterYears}`);
+let myAgeInDogYears = earlyYears + laterYears;
+//comment
+let myName = 'Ashlee';
+myName = myName.toUpperCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.}`)
